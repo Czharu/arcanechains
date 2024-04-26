9ad03491445bd23cb5fbdab22114193d1f0d89a2
Xuetenant <127169993+Xuetenant@users.noreply.github.com>
2024-04-26 21:10:16 +0100

    entries for tilemap prefabs

--- Assets/Scripts/Map Scripts/LevelGeneration.cs
+++ Assets/Scripts/Map Scripts/LevelGeneration.cs
@@ -3,170 +3,288 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //https://www.youtube.com/watch?v=nADIYwgKHv4
-public class LevelGeneration : MonoBehaviour {
+public class LevelGeneration : MonoBehaviour
+{
+    [System.Serializable]
+    public class RoomData
+    {
+        public Vector2 position;
+        public int type; // For example, 0 could mean a normal room, 1 could mean a special room, etc.
+        public bool up, down, left, right; // Door information
+        //One Door
+        public GameObject[] roomU_Prefabs; // Prefabs for rooms with an Up door only
+        public GameObject[] roomR_Prefabs; 
+        public GameObject[] roomD_Prefabs; 
+        public GameObject[] roomL_Prefabs; 
+        // Two Doors
+        public GameObject[] roomUR_Prefabs; 
+        public GameObject[] roomUD_Prefabs; 
+        public GameObject[] roomUL_Prefabs; 
+        public GameObject[] roomRD_Prefabs; 
+        public GameObject[] roomRL_Prefabs; 
+        public GameObject[] roomDL_Prefabs; 
+        //Three Doors
+        public GameObject[] roomURD_Prefabs; 
+        public GameObject[] roomURL_Prefabs;
+        public GameObject[] roomUDL_Prefabs;
+        public GameObject[] roomRDL_Prefabs;
+        //Four Doors
+        public GameObject[] roomURDL_Prefabs;       
+        
 
-    Vector2 worldSize = new Vector2(4,4);
+        public GameObject prefab;
+        // Add additional data as necessary
+    }
+    List<RoomData> generatedRoomData = new List<RoomData>();
+
+    Vector2 worldSize = new Vector2(4, 4);
     Room[,] rooms;
     List<Vector2> takenPositions = new List<Vector2>();
     int gridSizeX, gridSizeY, numberOfRooms = 20;
     public GameObject roomWhiteObj;
+    public GameObject[] roomPrefabs; // Array to hold various room prefabs
 
-    void Start() {
-    if (numberOfRooms >= (worldSize.x * 2) * (worldSize.y * 2)){
-        numberOfRooms = Mathf.RoundToInt((worldSize.x *2) * (worldSize.y * 2));
-    }
-    gridSizeX = Mathf.RoundToInt(worldSize.x);
-    gridSizeY = Mathf.RoundToInt(worldSize.y);
-    CreateRooms();
-    SetRoomDoors();
-    DrawMap();
+    void Start()
+    {
+        if (numberOfRooms >= (worldSize.x * 2) * (worldSize.y * 2))
+        {
+            numberOfRooms = Mathf.RoundToInt((worldSize.x * 2) * (worldSize.y * 2));
+        }
+        gridSizeX = Mathf.RoundToInt(worldSize.x);
+        gridSizeY = Mathf.RoundToInt(worldSize.y);
+        CreateRooms();
+        SetRoomDoors();
+        DrawMap();
+        InstantiateGameplayRooms();
     }
 
-    void CreateRooms(){
+    void CreateRooms()
+    {
         //setup
-        rooms = new Room[gridSizeX * 2,gridSizeY * 2];
-        rooms[gridSizeX,gridSizeY] = new Room(Vector2.zero, 1);
-        takenPositions.Insert(0,Vector2.zero);
+        rooms = new Room[gridSizeX * 2, gridSizeY * 2];
+        rooms[gridSizeX, gridSizeY] = new Room(Vector2.zero, 1);
+        takenPositions.Insert(0, Vector2.zero);
         Vector2 checkPos = Vector2.zero;
         //magic numbers to cratee leses clump or stuff
         float randomCompare = 0.2f, randomCompareStart = 0.2f, randomCompareEnd = 0.01f;
         //add rooms
-        for (int i =0; i < numberOfRooms -1; i++){
-            float randomPerc = ((float) i) / (((float)numberOfRooms - 1));
+        for (int i = 0; i < numberOfRooms - 1; i++)
+        {
+            float randomPerc = ((float)i) / (((float)numberOfRooms - 1));
             randomCompare = Mathf.Lerp(randomCompareStart, randomCompareEnd, randomPerc);
             //grab new position
             checkPos = NewPosition();
             //test new position
-            if (NumberOfNeighbors(checkPos, takenPositions) > 1 && Random.value > randomCompare){
-                int iterations =0;
-                do{
+            if (NumberOfNeighbors(checkPos, takenPositions) > 1 && Random.value > randomCompare)
+            {
+                int iterations = 0;
+                do
+                {
                     checkPos = SelectiveNewPosition();
                     iterations++;
 
-                }while(NumberOfNeighbors(checkPos, takenPositions) > 1 && iterations < 100);
+                } while (NumberOfNeighbors(checkPos, takenPositions) > 1 && iterations < 100);
                 if (iterations >= 50)
                     print("error: could not create with fewer neighbors than : " + NumberOfNeighbors(checkPos, takenPositions));
             }
             //finalize position
-            rooms[(int) checkPos.x + gridSizeX, (int) checkPos.y + gridSizeY] = new Room(checkPos, 0);
-            takenPositions.Insert(0,checkPos);
+            rooms[(int)checkPos.x + gridSizeX, (int)checkPos.y + gridSizeY] = new Room(checkPos, 0);
+            takenPositions.Insert(0, checkPos);
+            //
+
+            // Create and store room data instead of instantiating the room
+            RoomData newRoomData = new RoomData
+            {
+                position = checkPos,
+                type = DetermineRoomType(), // You'll create this method to determine the room type
+                prefab = roomPrefabs[DetermineRoomType()] // Use the method to index into the prefab array
+            };
+
+            generatedRoomData.Add(newRoomData);
         }
     }
 
-    Vector2 NewPosition(){
+    int DetermineRoomType()
+    {
+        // Logic to determine the room type
+        // For now, we'll just return a random room type
+        return Random.Range(0, roomPrefabs.Length);
+    }
+
+    public void InstantiateGameplayRooms() //You will need to call InstantiateGameplayRooms() when you want to generate the actual gameplay rooms, perhaps after the player finishes the planning phase or presses a 'Start Game' button.
+    {
+        foreach (RoomData roomData in generatedRoomData)
+        {
+            // Calculate the position where the room should be instantiated
+            Vector3 roomPosition = new Vector3(roomData.position.x * 100, roomData.position.y * 100, 0); // Multiply by 10 or room size
+
+            // Instantiate the room prefab at the calculated position
+            Instantiate(roomData.prefab, roomPosition, Quaternion.identity);
+        }
+    }
+
+    Vector2 NewPosition()
+    {
         int x = 0, y = 0;
         Vector2 checkingPos = Vector2.zero;
-        do{
+        do
+        {
             int index = Mathf.RoundToInt(Random.value * (takenPositions.Count - 1));
-            x = (int) takenPositions[index].x;
-            y = (int) takenPositions[index].y;
+            x = (int)takenPositions[index].x;
+            y = (int)takenPositions[index].y;
             bool UpDown = (Random.value < 0.5f);
             bool positive = (Random.value < 0.5f);
-            if (UpDown){
-                if (positive){
+            if (UpDown)
+            {
+                if (positive)
+                {
                     y += 1;
-                }else{
+                }
+                else
+                {
                     y -= 1;
                 }
-            }else{
-                if (positive){
+            }
+            else
+            {
+                if (positive)
+                {
                     x += 1;
-                }else{
+                }
+                else
+                {
                     x -= 1;
                 }
             }
-            checkingPos = new Vector2(x,y);
-        }while (takenPositions.Contains(checkingPos) || x >= gridSizeX || x < -gridSizeX || y >= gridSizeY || y < -gridSizeY);
+            checkingPos = new Vector2(x, y);
+        } while (takenPositions.Contains(checkingPos) || x >= gridSizeX || x < -gridSizeX || y >= gridSizeY || y < -gridSizeY);
         return checkingPos;
     }
 
-    Vector2 SelectiveNewPosition(){
+    Vector2 SelectiveNewPosition()
+    {
         int index = 0, inc = 0;
-        int x =0, y =0;
+        int x = 0, y = 0;
         Vector2 checkingPos = Vector2.zero;
-        do{
+        do
+        {
             inc = 0;
-            do{
-                index = Mathf.RoundToInt(Random.value * (takenPositions.Count -1));
-                inc ++;
+            do
+            {
+                index = Mathf.RoundToInt(Random.value * (takenPositions.Count - 1));
+                inc++;
 
-            }while (NumberOfNeighbors(takenPositions[index], takenPositions) > 1 && inc < 100);
-            x = (int) takenPositions[index].x;
-            y = (int) takenPositions[index].y;
+            } while (NumberOfNeighbors(takenPositions[index], takenPositions) > 1 && inc < 100);
+            x = (int)takenPositions[index].x;
+            y = (int)takenPositions[index].y;
             bool UpDown = (Random.value < 0.5f);
             bool positive = (Random.value < 0.5f);
-            if (UpDown){
-                if (positive){
+            if (UpDown)
+            {
+                if (positive)
+                {
                     y += 1;
-                }else{
+                }
+                else
+                {
                     y -= 1;
                 }
-            }else{
-                if (positive){
+            }
+            else
+            {
+                if (positive)
+                {
                     x += 1;
-                }else{
+                }
+                else
+                {
                     x -= 1;
                 }
             }
-            checkingPos = new Vector2(x,y);
-        }while (takenPositions.Contains(checkingPos) || x >= gridSizeX || x < -gridSizeX || y >= gridSizeY || y < -gridSizeY);
-        if (inc >= 100){
+            checkingPos = new Vector2(x, y);
+        } while (takenPositions.Contains(checkingPos) || x >= gridSizeX || x < -gridSizeX || y >= gridSizeY || y < -gridSizeY);
+        if (inc >= 100)
+        {
             print("Error: could not find position with only one neighbor");
         }
         return checkingPos;
     }
 
-    int NumberOfNeighbors(Vector2 checkingPos, List<Vector2> usedPositions){
-        int ret =0;
-        if (usedPositions.Contains(checkingPos + Vector2.right)){
+    int NumberOfNeighbors(Vector2 checkingPos, List<Vector2> usedPositions)
+    {
+        int ret = 0;
+        if (usedPositions.Contains(checkingPos + Vector2.right))
+        {
             ret++;
         }
-        if (usedPositions.Contains(checkingPos + Vector2.left)){
+        if (usedPositions.Contains(checkingPos + Vector2.left))
+        {
             ret++;
         }
-        if (usedPositions.Contains(checkingPos + Vector2.up)){
+        if (usedPositions.Contains(checkingPos + Vector2.up))
+        {
             ret++;
         }
-        if (usedPositions.Contains(checkingPos + Vector2.down)){
+        if (usedPositions.Contains(checkingPos + Vector2.down))
+        {
             ret++;
         }
         return ret;
     }
 
-    void SetRoomDoors(){
-        for (int x = 0; x < ((gridSizeX *2)); x++){
-            for (int y = 0; y < ((gridSizeY * 2)); y++){
-                if (rooms[x,y] == null){
+    void SetRoomDoors()
+    {
+        for (int x = 0; x < ((gridSizeX * 2)); x++)
+        {
+            for (int y = 0; y < ((gridSizeY * 2)); y++)
+            {
+                if (rooms[x, y] == null)
+                {
                     continue;
                 }
-                Vector2 gridPosition = new Vector2(x,y);
-                if (y - 1 < 0){ //checks above
-                    rooms[x,y].doorBot = false;
-                }else{
-                    rooms[x,y].doorBot = (rooms[x,y-1] != null);
+                Vector2 gridPosition = new Vector2(x, y);
+                if (y - 1 < 0)
+                { //checks above
+                    rooms[x, y].doorBot = false;
                 }
-                if (y + 1 >= gridSizeY * 2){ //checks bellow
-                    rooms[x,y].doorTop = false;
-                }else{
-                    rooms[x,y].doorTop = (rooms[x,y+1] != null);
+                else
+                {
+                    rooms[x, y].doorBot = (rooms[x, y - 1] != null);
                 }
-                if (x - 1 < 0){ //checks left
-                    rooms[x,y].doorLeft = false;
-                }else{
-                    rooms[x,y].doorLeft = (rooms[x - 1,y] != null);
+                if (y + 1 >= gridSizeY * 2)
+                { //checks bellow
+                    rooms[x, y].doorTop = false;
                 }
-                if (x + 1 >= gridSizeX * 2){ //checks right
-                    rooms[x,y].doorRight = false;
-                }else{
-                    rooms[x,y].doorRight = (rooms[x+1,y] != null);
+                else
+                {
+                    rooms[x, y].doorTop = (rooms[x, y + 1] != null);
+                }
+                if (x - 1 < 0)
+                { //checks left
+                    rooms[x, y].doorLeft = false;
+                }
+                else
+                {
+                    rooms[x, y].doorLeft = (rooms[x - 1, y] != null);
+                }
+                if (x + 1 >= gridSizeX * 2)
+                { //checks right
+                    rooms[x, y].doorRight = false;
+                }
+                else
+                {
+                    rooms[x, y].doorRight = (rooms[x + 1, y] != null);
                 }
             }
         }
     }
 
-    void DrawMap(){
-        foreach (Room room in rooms){
-            if (room == null){
+    void DrawMap()
+    {
+        foreach (Room room in rooms)
+        {
+            if (room == null)
+            {
                 continue;
             }
             Vector2 drawPos = room.gridPos;
