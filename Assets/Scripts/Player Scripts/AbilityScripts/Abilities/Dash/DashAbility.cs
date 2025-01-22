a02fb21322cb67eb4556de065ef88ac392e4636f
Czharu <henry.droste@gmail.com>
2025-01-22 02:19:41 +0100

    Tried fixing DashAbility, needs more work

--- Assets/Scripts/Player Scripts/AbilityScripts/Abilities/Dash/DashAbility.cs
+++ Assets/Scripts/Player Scripts/AbilityScripts/Abilities/Dash/DashAbility.cs
@@ -1,29 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Callbacks;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [CreateAssetMenu]
 public class DashAbility : Ability
 {
-    public float dashVelocity;
+    public float dashVelocity = 14f;
     public GameObject arrowPivot;
     public override void Activate(GameObject parent)
     {
-        Rigidbody2D rigidbody = parent.GetComponent<Rigidbody2D>();
-        Vector2 force = (faceMouse() - rigidbody.position).normalized * dashVelocity;
-        Debug.Log("DASHING");
-        rigidbody.AddForce(force);
+        Rigidbody2D rb = parent.GetComponent<Rigidbody2D>();
+        Vector2 direction = (faceMouse() - (Vector2)parent.transform.position).normalized;
+        Debug.Log("DASHING in direction: " + direction + " With intial velocity: " + rb.velocity);
+        rb.AddForce(direction * dashVelocity);
+        Debug.Log("Velocity after dash : " + rb.velocity);
     }
 
     private Vector2 faceMouse()
-        {
-
-        
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-
-        return mousePosition;
+    {  
+    Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+    return mousePosition;
     }
 }
 
